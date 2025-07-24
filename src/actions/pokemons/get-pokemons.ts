e3569d6c8a324a71d75a451b325a84c13420416a
heynar-max <heynar76@hotmail.com>
2025-07-23 21:04:46 -0500

    Interfaces de PokeAPI

--- src/actions/pokemons/get-pokemons.ts
+++ src/actions/pokemons/get-pokemons.ts
@@ -1,13 +1,22 @@
 import { pokeApi } from "../../config/api/pokeApi";
-import { Pokemon } from "../../domain";
+import type { Pokemon } from "../../domain";
+import type { PokeAPIPaginatedResponse, PokeAPIPokemon } from "../../infrastructure/interfaces/pokeapi.interfaces";
 
 
-export const getPokemons = async ( ): Promise<Pokemon[]> => {
+export const getPokemons = async (page: number, limit: number = 20): Promise<Pokemon[]> => {
 
     try {
-        const url = `/pokemon`;
-        const {data} = await pokeApi.get(url);
+        const url = `/pokemon?offset=${ page * 10}&limit=${limit}`;
+        const {data} = await pokeApi.get<PokeAPIPaginatedResponse>(url);
 
+        const pokemonPromises = data.results.map(info => {
+            return pokeApi.get<PokeAPIPokemon>(info.url);
+        });
+
+        const pokeApiPokemons = await Promise.all(pokemonPromises);
+        
+
+        console.log(pokeApiPokemons)
         console.log(data)
 
         return [];

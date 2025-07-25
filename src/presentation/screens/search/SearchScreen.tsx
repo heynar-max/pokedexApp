2c5b110f4b50691c47517980bab0c27a74627f98
heynar-max <heynar76@hotmail.com>
2025-07-25 15:57:01 -0500

    Filtrar listado por id y nombre

--- src/presentation/screens/search/SearchScreen.tsx
+++ src/presentation/screens/search/SearchScreen.tsx
@@ -1,4 +1,4 @@
-import { View } from "react-native"
+import { Text, View } from "react-native"
 import { ActivityIndicator, TextInput } from "react-native-paper";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { globalTheme } from "../../../config/theme/global-theme";
@@ -7,16 +7,37 @@ import { PokemonCard } from "../../components/pokemons/PokemonCard";
 import { Pokemon } from "../../../domain";
 import { useQuery } from "@tanstack/react-query";
 import { getPokemonNamesWithId } from "../../../actions";
+import { useMemo, useState } from "react";
 
 
 export const SearchScreen = () => {
 
     const {top} = useSafeAreaInsets();
+    const [term, setTerm] = useState('');
 
     const {isLoading, data: pokemonNameList = []} = useQuery({
         queryKey: ['pokemons', 'all'],
         queryFn: () => getPokemonNamesWithId(),
     });
+
+    // Todo: aplicar debounce
+    const pokemonNameIdList = useMemo(() => {
+        // Es un número
+        if (!isNaN(Number(term))) {
+        const pokemon = pokemonNameList.find(
+            pokemon => pokemon.id === Number(term),
+        );
+        return pokemon ? [pokemon] : [];
+        }
+
+        if (term.length === 0) return [];
+        if (term.length < 3) return [];
+
+        return pokemonNameList.filter(pokemon =>
+        pokemon.name.includes(term.toLocaleLowerCase()),
+        );
+    }, [term]);
+
 
     return (
         <View style={[ globalTheme.globalMargin, {paddingTop: top}]}>
@@ -25,11 +46,12 @@ export const SearchScreen = () => {
                 mode="flat"
                 autoFocus
                 autoCorrect={false}
-                onChangeText={value => console.log(value)}
-                value={''}
+                onChangeText={setTerm}
+                value={term}
             />
         <ActivityIndicator style={{ paddingTop: 20}}/>
 
+        <Text>{JSON.stringify(pokemonNameIdList, null, 2)}</Text>
         <FlatList
             data={[] as Pokemon[]}
             keyExtractor={(pokemon, index) => `${pokemon.id}-${index}`}
